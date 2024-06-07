7d3c02f0408fe5c23210a5360b2bed3c401b86db
abcLiyew <18378500963@163.com>
2024-06-08 02:16:37 +0800

    Initial commit

---
+++ src/main/java/com/example/TemplateModClient.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * ClassName : TemplateModClient
+ * Package : com.example
+ * Description :
+ *
+ * @author 饿死的流浪猫<br>
+ * <b>创建日期</b><br> 2024/6/8 2:03
+ * @version 1.0.1
+ */
+public class TemplateModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
